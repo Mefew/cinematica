b457330bfcd81a2185218b4d9489c6527e6a2dda
Mefew <matheus_i@hotmail.com>
2016-09-14 15:16:11 -0300

    git commit

---
+++ core/src/br/cefetmg/games/movement/behavior/Fugir.java
@@ -0,0 +1,46 @@
+package br.cefetmg.games.movement.behavior;
+
+import br.cefetmg.games.movement.Direcionamento;
+import br.cefetmg.games.movement.Pose;
+import com.badlogic.gdx.math.Vector3;
+
+/**
+ *
+ * @author Flávio Coutinho <fegemo@gmail.com>
+ */
+public class Fugir extends Algoritmo {
+
+    private static final char NOME = 'f';
+
+    public Fugir(float maxVelocidade) {
+        this(NOME, maxVelocidade);
+    }
+
+    protected Fugir(char nome, float maxVelocidade) {
+        super(nome);
+        this.maxVelocidade = maxVelocidade;
+
+    }
+
+    @Override
+    public Direcionamento guiar(Pose agente) {
+        Direcionamento output = new Direcionamento();
+
+        Vector3 aux = new Vector3(alvo.getObjetivo());
+        // Calcula a direção para onde o agente deve ir
+        // Input: a posicao do objetivo (this.alvo) e deste agente
+        // ..
+        // ..
+        // ..
+        // ..
+        // Output: vetor velocidade (apontando na direção encontrada) 
+        //         limitado pela maxVelocidade (tangencial)
+        // PS: não é necessário definir a rotação
+        output.velocidade = aux.sub(agente.posicao);
+        output.velocidade = output.velocidade.nor();
+        output.velocidade = output.velocidade.scl(-maxVelocidade);
+        
+        return output;
+    }
+
+}
